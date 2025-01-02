378f2ab83d5a05c6720137019d1d8b746be7eb0d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2025-01-02 15:11:38 +0300

    follow completed

--- src/controllers/follow.js
+++ src/controllers/follow.js
@@ -2,6 +2,7 @@
 
 const Follow = require("../models/follow");
 const User = require("../models/user");
+const { update } = require("./comment");
 
 module.exports = {
   list: async (req, res) => {
@@ -15,21 +16,39 @@ module.exports = {
   },
 
   create: async (req, res) => {
-    console.log(req.body.followerId);
-
     const newFollow = await Follow.create({
       followingId: req.user._id,
       followerId: req.body.followerId,
     });
 
+    const { username, profileImage } = await User.findOne({
+      _id: req.body.followerId,
+    });
+
     await User.updateOne(
       { _id: req.body.followerId },
-      { $push: { followers: req.user._id } }
+      {
+        $push: {
+          followers: {
+            userId: req.user._id,
+            username: req.user.username,
+            profileImage: req.user?.profileImage,
+          },
+        },
+      }
     );
 
     await User.updateOne(
       { _id: req.user._id },
-      { $push: { following: req.body.followerId } }
+      {
+        $push: {
+          following: {
+            userId: req.body.followerId,
+            username: username,
+            profileImage: profileImage,
+          },
+        },
+      }
     );
 
     res.status(201).send({
@@ -38,4 +57,72 @@ module.exports = {
     });
   },
 
+  read: async (req, res) => {
+    const data = await Follow.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await Follow.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+
+    res.status(202).send({
+      error: false,
+      data,
+    });
+  },
+
+  delete: async (req, res) => {
+    try {
+      const { followerId } = req.body;
+
+      // Follow kaydını sil
+      const follow = await Follow.findOneAndDelete({
+        followingId: req.user._id,
+        followerId,
+      });
+
+      if (!follow) {
+        return res.status(404).send({
+          error: true,
+          message: "Follow record not found",
+        });
+      }
+
+      // Takip eden kullanıcının following listesinden çıkar
+      await User.updateOne(
+        { _id: req.user._id },
+        {
+          $pull: {
+            following: { userId: followerId },
+          },
+        }
+      );
+
+      // Takip edilen kullanıcının followers listesinden çıkar
+      await User.updateOne(
+        { _id: followerId },
+        {
+          $pull: {
+            followers: { userId: req.user._id },
+          },
+        }
+      );
+
+      res.status(200).send({
+        error: false,
+        message: "Unfollow successful",
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: error.message,
+      });
+    }
+  },
 };
--- src/routes/follow.js
+++ src/routes/follow.js
@@ -6,4 +6,11 @@ const follow = require("../controllers/follow");
 
 router.route("/").get(follow.list).post(follow.create);
 
+router
+  .route("/:id")
+  .get(follow.read)
+  .put(follow.update)
+  .patch(follow.update)
+  .delete(follow.delete);
+
 module.exports = router;
