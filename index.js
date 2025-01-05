58bdf8868127633fe8c8acb0929b9fe67d8309da
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2025-01-05 23:19:06 +0300

    morgan added

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.use(express.json());
 
 app.use(require("./src/middlewares/authentication"));
 
+app.use(require("./src/middlewares/logger"));
+
 app.use(require("./src/middlewares/queryHandler"));
 
 /*------------------------------------*/
